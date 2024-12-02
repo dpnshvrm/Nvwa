fc31e287ebf06bf7356c5a3f3153248cda7128de
ZhengzhiTan <66708716+ZhengzhiTan@users.noreply.github.com>
2024-12-02 15:23:02 -0500

    Add files via upload

---
+++ new attemp/0_preproc_dataset/Subset_Oryza/TPE-lr4-epoch10/TPE-lr4-epoch10.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --job-name=TPE
+#SBATCH --cpus-per-task=8
+#SBATCH --partition=sm-gpu-1
+#SBATCH --time=120:00:00
+#SBATCH --mem=500G
+#SBATCH --output=/data2/mukhtar_lab/tzhengz/new_attemp/0_preproc_dataset/subset_Oryza/train/TPE-lr4-epoch10/test.out
+#SBATCH --error=/data2/mukhtar_lab/tzhengz/new_attemp/0_preproc_dataset/subset_Oryza/train/TPE-lr4-epoch10/test.err
+#SBATCH --mail-type=all
+#SBATCH --mail-user=tzhengz@clemson.edu
+
+python /data2/mukhtar_lab/tzhengz/new_attemp/0_preproc_dataset/subset_Oryza/train/0_hyperopt_BCE_TPE.py 0 /data2/mukhtar_lab/tzhengz/new_attemp/0_preproc_dataset/subset_Oryza/data/subset_Oryza_train_test_10000bp.h5
